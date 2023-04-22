042e9f1faaf76b169276dc497fa8149e1bf95e05
AkionGarro <carloscamp1008@estudianTEC.cr>
2023-04-22 15:20:49 -0600

    Update Documatation

--- Project/App/src/app/test/user.spec.ts
+++ Project/App/src/app/test/user.spec.ts
@@ -1,23 +1,31 @@
 import { User } from './user';
 
-describe('User', () => {
-  it('should create an instance', () => {
-    expect(new User()).toBeTruthy();
-  });
-});
+
 
 describe('Casos de prueba con asignar el email', function () {
   let component: User;
   beforeEach(() => {
     component = new User();
   });
-  // Prueba 1: Verificar que el correo sea válido y se asigne correctamente
+
+  
+  // Caso de prueba 1: Asignar email valido
+  // Objetivo: Verificar que el método setEmail asigne correctamente el valor del correo electrónico si el correo es válido.
+  // Datos de prueba:
+  // email: "usuario@dominio.com"
+  // Resultado esperado:"usuario@dominio.com"
+  // El método setEmail debe asignar el valor "usuario@dominio.com" al atributo this.email.
   it('Asignar email valido', () => {
     component.setEmail('usuario@dominio.com');
     expect(component.getEmail()).toEqual('usuario@dominio.com');
   });
 
-  // Prueba 2: Verificar que el correo sea inválido y se lance un error
+  // Caso de prueba 2: Asignar email invalido
+  // Objetivo: Verificar que el método setEmail lance un error si el correo no es válido.
+  // Datos de prueba:
+  // email: "correo_invalido"
+  // Resultado esperado: 'El formato del correo es incorrecto'
+  // El método setEmail debe lanzar un error con el mensaje "El formato del correo es incorrecto".
   it('Asignar email invalido', () => {
     expect(() => {
       component.setEmail('correo_invalido');
@@ -30,27 +38,44 @@ describe('Casos de prueba de la contraseña', function () {
   beforeEach(() => {
     component = new User();
   });
-  // Prueba 1: Asignar una contraseña válida
+
+  // Caso de prueba 3: Asignar contraseña valido
+  // Objetivo: Verificar que una contraseña válida sea aceptada por el método
+  // Datos de prueba:
+  // contraseña: "Carlos12334"''
+  // Resultado esperado: true
   it('Asignar contraseña valida', () => {
     expect(component.validatePassword('Carlos12334')).toEqual(true);
   });
 
-  //Prueba 2: Verificar que el correo sea inválido y se lance un error
-  it('Asignar contraseña de poca longitud', () => {
+  // Caso de prueba 4: Rechazar contraseña de poca longitud
+  // Objetivo: Verificar que una contraseña tenga por lo menos 10 caracteres
+  // Datos de prueba:
+  // contraseña: 'Car12'
+  // Resultado esperado: 'La contraseña necesita al menos 10 caracteres'
+  it('Rechazar contraseña de poca longitud', () => {
     expect(() => {
       component.validatePassword('Car12');
     }).toThrowError('La contraseña necesita al menos 10 caracteres');
+    });
   });
-});
 
-describe('Casos de tiene letras en una cadena', function () {
+describe('Casos de prueba que contenga letras en una cadena', function () {
   let component: User;
   beforeEach(() => {
     component = new User();
   });
 
-  // Prueba 1: Verificar que la cadena tenga letras
-  it('Tiene letras la cadena ingresada', () => {
+  // Objetivo: verificar que el método tieneLetras() retorne true para un texto que contiene letras .
+  // Datos de prueba: 
+  //text 1 = '1234' 
+  //text 2 = 'aasd643531'
+  //text 3 = '234234'
+  //Resultado esperado:
+  //res 1 = false
+  //res 2 = true 
+  //res 3 = false
+  it('Comprobar si hay letras', () => {
     [
       { text: '1234', res: false },
       { text: 'aasd643531', res: true },
@@ -59,4 +84,11 @@ describe('Casos de tiene letras en una cadena', function () {
       expect(component.tieneLetras(text)).toBe(res);
     });
   });
+
+  afterEach(() => {
+    component =
+  });
+
+
+
 });
